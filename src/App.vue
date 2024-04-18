863d760bdf237a7a376ea49a6b5add3f6894a4ff
Moh Iqbal Rafsanjani (PKWT) <moh_ir.i@bi.go.id>
2024-04-18 11:17:35 +0700

    First commit vue js

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>To-Do List</h1>
+    <ul>
+    <li v-for="item in ToDoItems" :key="item.id">
+      <to-do-item :label="item.label" :done="item.done" :id="item.id"></to-do-item>
+    </li>
+  </ul>
+  </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoItem from "./components/ToDoItem.vue";
+// import uniqueId from "lodash";s
 
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
-  }
-}
+    ToDoItem,
+  },
+  data() {
+    return {
+      ToDoItems: [
+      { 
+        // id: uniqueId("todo-"), 
+        label: "Learn Vue", done: false },
+        {
+          // id: uniqueId("todo-"),
+          label: "Create a Vue project with the CLI",
+          done: true,
+        },
+        { 
+          // id: uniqueId("todo-"), 
+          label: "Have fun", done: true },
+        { 
+          // id: uniqueId("todo-"), 
+          label: "Create a to-do list", done: false },
+      ],
+    };
+  },
+};
+
 </script>
 
 <style>
